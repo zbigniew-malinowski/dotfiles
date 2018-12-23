f34c747d553fb22e73f03b2c073165558bf97c99
Zbigniew Malinowski <zbigniew.malinowski@gmail.com>
2018-12-23 19:05:54 +0100

    install and initial config

---
+++ bin/install.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+if [[ -d ~/.dotfiles ]]; then
+    echo ".dotfiles already exists, skipping setup"
+    exit
+fi
+
+git clone https://github.com/zbigniew-malinowski/dotfiles ~/.dotfiles
+cd ~/.dotfiles
+git submodule add https://github.com/zsh-users/antigen.git antigen
+
+if [[ -L ~/.zshrc ]] ; then
+    echo "removing old link"
+    rm ~/.zshrc
+fi
+
+if [[ -e ~/.zshrc ]] then
+  echo "backing up old .files"
+  mkdir ~/.dotfiles.old
+  cp ~/.zshrc ~/.dotfiles.old/.zshrc
+fi
+
+echo "linking .files"
+ln ~/.dotfiles/zshrc ~/.zshrc
+
+echo "setting zsh as the default shell"
+chsh -s $(which zsh)
---
+++ zshrc
@@ -0,0 +1,17 @@
+ZSHA_BASE=$HOME/.dotfiles
+source $ZSHA_BASE/antigen/antigen.zsh
+
+antigen-use oh-my-zsh
+antigen-bundle git
+
+if [ "$OSTYPE"="darwin11.0" ]; then
+  antigen-bundle osx
+fi
+
+antigen-bundle vagrant
+antigen-bundle zsh-users/zsh-syntax-highlighting
+antigen-bundle zsh-users/zsh-history-substring-search
+
+antigen-theme
+
+antigen-apply
